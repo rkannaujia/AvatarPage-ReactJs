f1bf7fa02c2b9866e0f83608e2fd11e8816a37cd
Rahul Kannaujia <rkannaujia96@gmail.com>
2021-08-05 14:24:32 -0700

    Added some CSS and modified both avtar.js and avtarlist.js file

--- src/Avatar.js
+++ src/Avatar.js
@@ -4,13 +4,54 @@ import 'tachyons';
 import Avatarlist from './Avtarlist.js';
 
 const Avatar = (props) => {
+
+    const avatarlistarray = [
+        {
+            id: 1,
+            name: "Lucy",
+            work: "Web developer"
+        },
+        {
+            id: 2,
+            name: "Yogesh Joglekar",
+            work: "IT Associate"
+        },
+        {
+            id: 3,
+            name: "Samson",
+            work: "School Leader"
+        },
+        {
+            id: 4,
+            name: "Monali Topare",
+            work: "Socail worker"
+
+        }
+    ]
+
+    const arrayAvatarCard = avatarlistarray.map((avatarcard, index) => {
+        return <Avatarlist id={avatarlistarray[index].id}
+            name={avatarlistarray[index].name}
+            work={avatarlistarray[index].work} />
+    })
     return (
-        <div>
+        <div className="mainpage">
             <h1>Welcome to Avatar world</h1>
-            <Avatarlist id="1" name="Rahul Kannaujia" work ="Web developer" />
-            <Avatarlist id="2" name="Yogesh Joglekar" work ="IT Associate" />
-            <Avatarlist id="3" name="Rashmi Chainani" work ="School Leader" />
-            <Avatarlist id="4" name="Monali Topare" work ="Socail worker" />
+        
+            {/* <Avatarlist id={avatarlistarray[0].id}
+                name={avatarlistarray[0].name}
+                work={avatarlistarray[0].work} />
+            <Avatarlist id={avatarlistarray[1].id}
+                name={avatarlistarray[1].name}
+                work={avatarlistarray[1].work} />
+            <Avatarlist id={avatarlistarray[2].id}
+                name={avatarlistarray[2].name}
+                work={avatarlistarray[2].work} />
+            <Avatarlist id={avatarlistarray[3].id}
+                name={avatarlistarray[3].name}
+                work={avatarlistarray[3].work} /> */}
+
+                {arrayAvatarCard}
             <button> Subcribe </button>
 
         </div>
--- src/Avtarlist.js
+++ src/Avtarlist.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Avatarlist = (props) => {
     return (
-        <div className="avatarstyle ma4 bg-light-purple dib pa3 tc grow ">
+        <div className="avatarstyle ma3 bg-light-purple dib pa2 tc grow shadow-4">
             <img src={`https://joeschmoe.io/api/v1/${props.name}`} alt="Avatar"></img>
             <h1 className>{props.name}</h1>
             <p>{props.work}</p>
